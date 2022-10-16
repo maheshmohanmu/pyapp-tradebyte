61938369d5e7f2e014767c281eccd64b0fa5521b
Mahesh Mohan <maheshmohan@Maheshs-MacBook-Air.local>
2022-10-16 12:00:05 +0530

    unit test fail etst

--- tests/test.py
+++ tests/test.py
@@ -11,7 +11,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertFalse('Foo'.isupper())
 
     def test_split(self):
-        s = 'hello world'
+        s = '2'
         self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
